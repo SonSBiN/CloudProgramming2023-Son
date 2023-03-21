40ede9a3ca90d4500239a00e914efccb50be9704
SonSBiN <victor990914@naver.com>
2023-03-21 17:24:20 +0900

    글 상세보기 만들었다네

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def index(request):
     posts = Post.objects.all().order_by('-pk')
 
     return render(request, 'blog/index.html', {'posts': posts, })
+
+
+def single_post_page(request, post_num):
+    post = Post.objects.get(pk=post_num)
+
+    return render(request, 'blog/single_post_page.html', {'post': post, })
