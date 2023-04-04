8ae2976d944843b2f38ee2dc5e736ac71dd7acf2
SonSBiN <victor990914@naver.com>
2023-04-04 15:40:13 +0900

    카테고리 추가

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,16 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    name = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
 
     def __str__(self):
         return self.name
+
+
+class Meta:
+    verbose_name_plural: 'Categories'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -21,6 +26,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
